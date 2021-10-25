45bd76b70e30550e901442d1d0d7321754a5b260
Mark Burggraf <markb@mantisbible.com>
2021-10-25 11:15:46 -0700

    init

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,36 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, useIonAlert } from '@ionic/react';
+
 import ExploreContainer from '../components/ExploreContainer';
+
 import './Home.css';
 
 const Home: React.FC = () => {
+  const [presentAlert] = useIonAlert()
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>ionic-react-alert-bug</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent className="ion-padding ion-text-center">
+
+      <IonButton
+								color='danger'
+								fill='outline'
+								onClick={() =>
+									presentAlert({
+										cssClass: 'my-css',
+										header: 'Test',
+										message: 'If you see this, it\'s working.',
+										buttons: ['Cancel', { text: 'OK', handler: (d) => console.log('OK') }],
+										onDidDismiss: (e) => console.log('did dismiss'),
+									})
+								}>
+								<strong>TEST ALERT</strong>
+							</IonButton>
+
       </IonContent>
     </IonPage>
   );
